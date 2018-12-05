74adc6b57408f62e3f0238aef195b37aa74c4ae7
OffWorlder <ptr.e.booth@gmail.com>
2018-12-05 00:27:56 -0800

    Day 1 Part 2 - experimenting with iterators and back_inserter

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -1,12 +1,41 @@
 // Day1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+//
 
 #include "pch.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <unordered_set>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 using namespace std;
+
+void day1part2_withIterators()
+{
+	ifstream inFile("input.txt");
+	vector<int> numbers;
+	numbers.reserve(1000);
+	istream_iterator<int> eos;              // end-of-stream iterator
+	istream_iterator<int> iit(inFile);
+	copy(iit, eos, back_inserter(numbers));
+	int total = 0;
+	unordered_set<int> answerFinder{ total };
+	while (true)
+	{
+		for (auto number:numbers)
+		{
+			total += number;
+			const auto [_, isInserted] = answerFinder.insert(total); // wow! structured bindings, boo! no std:ignore anymore
+			if (!isInserted)
+			{
+				cout << total << endl;
+				return;
+			}
+		}
+	}
+}
 
 void day1part2()
 {
@@ -19,7 +48,7 @@ void day1part2()
 		while (inFile >> number)
 		{
 			total += number;
-			const auto [_, isInserted] = answerFinder.insert(total); // wow! structured bindings
+			const auto[_, isInserted] = answerFinder.insert(total); // wow! structured bindings, boo! no std:ignore anymore
 			if (!isInserted)
 			{
 				cout << total << endl;
@@ -45,17 +74,9 @@ void day1part1()
 
 int main()
 {
+	// todo experiment with timers
 	//day1part1();
-	day1part2();
+	//day1part2();
+	day1part2_withIterators();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
