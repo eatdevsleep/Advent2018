a2a50ad97ad7425c3809cfd79112cff7696c16ee
OffWorlder <ptr.e.booth@gmail.com>
2018-12-05 23:38:50 -0800

    Advent 2018 Day 2

--- Day2/Part1.h
+++ Day2/Part1.h
@@ -1,12 +1,49 @@
 #pragma once
+
+#include <iostream>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
 class Part1
 {
 public:
 
+  static std::pair<bool, bool> CheckSum(std::string& code)
+  {
+    int stringln = code.length();
+    bool two = false;
+    bool three = false;
+    std::sort(code.begin(), code.end());
+    int count = 1;
+    for (int i = 1; i < stringln & (!two|!three); ++i)
+    {
+      if (code[i] == code[i - 1])
+        ++count;
+      else
+      {
+        if (count == 2) two = true;
+        if (count == 3) three = true;
+        count = 1;
+      }
+    }
+    return std::make_pair(two, three);
+  }
 
   static void Run()
   {
-    // TODO: Add your implementation code here.
+    std::ifstream inFile("input.txt");
+    int twos = 0;
+    int threes = 0;
+    std::string code;
+    while (inFile >> code)
+    {
+      const auto[aTwo, aThree] = CheckSum(code);
+      twos += aTwo;
+      threes += aThree;
+    }
+    std::cout << twos * threes << std::endl;
   }
 };
 
--- Day2/Part2.h
+++ Day2/Part2.h
@@ -1,10 +1,56 @@
 #pragma once
+#include <iostream>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iterator>
+#include <unordered_set>
+using namespace std;
 class Part2
 {
 public:
+  
+
   static void Run()
   {
-    // TODO: Add your implementation code here.
+    ifstream inFile("input.txt");
+    vector<string> codes;
+    codes.reserve(1000);
+    istream_iterator<string> eos;              // end-of-stream iterator
+    istream_iterator<string> iit(inFile);
+    copy(iit, eos, back_inserter(codes));
+
+    unordered_set<string> hits;
+    bool iterationHasLeft = true;
+    for (int charMissing = 0; iterationHasLeft; ++charMissing)
+    {
+      hits.clear();
+      iterationHasLeft = false;
+
+      auto end = hits.end();
+      for (auto& code : codes)
+      {
+        int codeLength = code.length();
+        if (codeLength > charMissing+1)
+        {
+          int secondStringLength = codeLength - charMissing - 1;
+          auto subString = code.substr(0, charMissing).append(code.substr(charMissing + 1, secondStringLength));
+          
+          if (auto iter = hits.find(subString);  iter == end)
+          {
+            iterationHasLeft = true;
+            hits.insert(subString);
+          }
+          else
+          {
+            iterationHasLeft = true;
+            cout << subString << endl;
+          }
+        }
+      }
+    }
   }
 };
 
